da39394ca253a322845497db663807e76642bb20
Sean Champ <spchamp@users.noreply.github.com>
2021-11-09 00:17:31 -0800

    [rbproject split] zmulti-rb: Adding earlier AssocHash updates; updating options.rspec
    assochash.rb
    - Defining constants, classes in a module AsoscHash
    - Adding new exception/error classes
    - Adding rdoc
    
    options.rspec
    - Adding additional tests

--- assochash.rb
+++ assochash.rb
@@ -1,11 +1,61 @@
 ## assochash.rb - Hash with configurable key evaluation
 
-module AssocHashConstants
+## *AssocHash( namespace
+module AssocHash
   THIS = lambda { |a| a.itself }
 end
 
+## Base class for *AssocHash* key exception classes
+class AssocHash::KeyException < RuntimeError
+
+  ## key value for the exception
+  attr_reader :key
+
+  ## container context for the provided key value
+  attr_reader :container
+
+  ## Base class constructor
+  ## @key [any] Key value for the exception
+  ## @container[AssocHash] container context of this *KeyException*
+  ## @msg [String] message for the exception. A generic default value
+  ##  is provided, for purpose of convenience
+  def initialize (key, container,
+                  msg = "KeyException: #{key} in #{container}" )
+    super(msg)
+    @key = key
+    @container = container
+  end
+
+end
+
+## Error class for "Key not found' exceptions
+class AssocHash::KeyNotFoundError < AssocHash::KeyException
+  def initialize(key, container,
+                 msg = "Key not found: #{key} in #{container}")
+    super
+  end
+end
+
+## Error class for attempted overwrite of a bound value
+## in an *AssocHash*
+class AssocHash::KeyOverwriteError < AssocHash::KeyException
+  attr_reader :stored_object
+  attr_reader :new_object
+
+  def initialize(key, stored, new, container,
+                 msg = "Unable to store #{new} for key #{key} in #{container}. Object already stored: #${stored}})")
+    super(key,container,msg)
+    @stored_object = stored
+    @new_object = new
+  end
+end
+
+
 require('forwardable')
-class AssocHash
+## Hash delegate class, for storing objects of
+## a single type using a hash key value computed
+## from a key_proc function on each value in the hash
+class AssocHash::AssocHash
 
   extend(Forwardable)
   include(Enumerable)
@@ -13,7 +63,7 @@ class AssocHash
   ##
   ## The method AssocHash.add(..) would be used to add elements to the
   ## delegated hash @table, with conditional checking to prevent storage
-  ## of objects having a duplicate key under the @keytest proc.
+  ## of objects having a duplicate key under the @key_proc proc.
   ##
   ## While the :[]= method may represent a familiar method name for
   ## collections in Ruby, it would need at least an additional arg
@@ -25,40 +75,83 @@ class AssocHash
                  :length, :empty?,
                  :include?, :has_key?, :key?, :member?)
 
+
+  ## Convenience method for initialization of the encapsulated Hash
+  ## object, under the *AssocHash* constructor, such that the default
+  ## <em>default proc</em> for the encapsulated **Hash** would raise a
+  ## *KeyNotFoundError*
+  ##
+  ## @param whence [AssocHash] context of the returned function
+  ## @return [lambda] lamda form to use a default <em>default proc</em>
+  ##  for the encapsulated Hash object, raising a *KeyNotFoundError*
   def self.table_default(whence)
-    ## NB convenience method for a more informative exception message
-    ## under #initialize, such that the exception message may reference
-    ## the AssocHash encapsulating the referring hash table
-    ##
-    ## FIXME need to define a new exception class, to include the
-    ## AssocHash in the exception object, for purpose of debugging ...
-    ##
     return lambda { |hash, key|
-      raise("Found no key #{key} in #{whence.class} #{whence.object_id}")
+      raise new KeyNotFoundError(key, whence)
     }
   end
 
+  ## convenience method for class defaults
+  ## @param whence [AssocHash] context of the returned function
+  ## @return [lambda] lamda form to use when testing for object
+  ##  overwrite in the *AssocHash*. This function will raise a
+  ##  KeyOverwriteError
   def self.overwrite_default(whence)
-    ## NB another convenience method for class defaults
+    ## NB
     ##
-    ## FIXME this needs a more specific conditions API
+    ## - 'whence' would be of type AssocHash
+    ## - 'obj' in the lambda would represent an object that
+    ##   was to to be stored in whence, while an object
+    ##   is already stored for 'key'
+    ##
+    ## This lambda would be called during a call to
+    ## AssocHash#add, such that an object was provided to
+    ## the call and that object is inequivalent to an
+    ## object already stored in the AssocHash denoted by
+    ## whence.
+    ##
+    ## In the general case of a lambda as an overwrite_proc
+    ## for an AssocHash, If the lambda returns false or nil
+    ## or performs a non-local exit (e.g with raise or throw)
+    ##  then the new object -- here denoted by obj -- will
+    ## not be stored for the key value. If this function
+    ## returns a non-falsey value, the the object will be
+    ## stored for that key value.
+    ##
+    ##
+    ## This class method is used as a utility during
+    ## AssocHash#initialize, in providng a default lambda
+    ## for the  'ovewrite' parameter to the constructor.
+    ## The default AssocHash#ovewrite_proc lambda would thus
+    ## raise an error, preventing overwrite under any equivalent
+    ## key value
+    ##
     return lambda { |key, obj|
-      raise("An object is already registered for key #{key} \
-in #{whence.class} #{whence.object_id}") }
+      raise new KeyOverwriteError(key, whence.get[key], obj, whence)
+    }
   end
 
-  def initialize(keytest: AssocHashConstants::THIS,
+
+  ## Create a new *AssocHash*
+  ## @param key_proc [lambda] Function to call for computing
+  ##  an object's key value, as when adding an object to
+  ##  this AssocHash
+  ## @param default [any]
+  ## @param overwrite [lambda]
+  def initialize(key_proc: AssocHash::THIS,
                  default: self.class.table_default(self),
                  overwrite: self.class.overwrite_default(self))
     @table = Hash.new(&default)
-    @keytest = keytest
+    @key_proc = key_proc
     @overwrite_proc = overwrite
   end
 
 
-  def add(obj, overwrite = false)
+  ## Cdd the provided object to this AssocHash
+  ##
+  ## This assumes that the 'obj' will respond to the *key_proc* form
+  def add(obj)
     ## NB if an object added to an AssocHash is modified later, such
-    ## that it would no longer produce the same value under the @keytest
+    ## that it would no longer produce the same value under the @key_proc
     ## proc, such a change may result in unexpected behaviors due to the
     ## subsequent disparity of the object's active key value, compared
     ## to the object's storage under the AssocHash
@@ -69,32 +162,41 @@ in #{whence.class} #{whence.object_id}") }
     if @table.member?(usekey)
       if (obj == @table[usekey])
         return obj
-      elsif ! overwrite
-        ## if the overwrite proc returns a local non-true value,
-        ## then return before overwrite
-        if ! @overwrite_proc.call(usekey,obj)
-          return false
-        end
+      elsif ! @overwrite_proc.call(usekey,obj)
+        return false
       end
     end
     @table[usekey]=obj
-    return obj
   end
 
+  ## may raise an exception, per the 'default proc' configuration
+  ## of the encapsulated hash table
+  ##
+  ## @see Hash.include?
   def get(key)
     ## NB convenience method
     ##
     ## NB this and the :[] delegate method may both result in a raised
-    ## exception, per how @table has been initialized
+    ## exception, e.g if the @table has been initialized under this
+    ## assoc hash such that the default lambda (proc) for the table
+    ## would raise an error. This would be the default behavior, under
+    ## the AssocHash constructor
     ##
-    ## see also: #member?
+    ## see also: the #member? delegate method on AssocHash
     return @table[key]
   end
 
-  def key(obj)
-    ## NB utility method, does not imply membership for the object in this AssocHash
-    @keytest.call(obj)
-  end
+
+    ## utility method, does not imply membership for the object in this AssocHash
+    def key(obj)
+      @key_proc.call(obj)
+    end
+
+    protected
+
+    attr_reader :overwrite_proc
+    attr_reader :key_proc
+    attr_reader :table
 
 end
 
