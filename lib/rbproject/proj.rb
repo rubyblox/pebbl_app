bb35d96fb4ea5bb53971ac17049fd5d114a78c25
Sean Champ <spchamp@users.noreply.github.com>
2021-11-16 02:33:26 -0800

    [rbproject split] Checking in proj.rb version 0.4.1

--- lib/rbproject/proj.rb
+++ lib/rbproject/proj.rb
@@ -2,6 +2,8 @@
 
 require('psych')
 
+
+
 class FieldDesc
   attr_reader :in_class
   attr_reader :reader
@@ -10,8 +12,6 @@ class FieldDesc
   def initialize(in_class, reader_name: nil,
                  writer_name: nil)
     @in_class = in_class
-
-    ## FIXME cannot call 'send' on actual method objects
 
     @reader = reader_name
     if (writer_name == true) && reader_name
@@ -26,14 +26,35 @@ end
 
 class Proj
 
+  VERSION="0.4.1"
+
+  ## FIXME the setter for Psych.dump_tags[Proj]
+  ## was not being evaluated when in an END
+  ## block in this file. Not any END block
+  ## in this file was being evaluated
+  DUMP_TAG= Psych.dump_tags[self] ||=
+            "#{self.name}@#{self::VERSION}"
+
   def self.load_yaml(file)
 
-    p = Proj.allocate
+    descs = LOADABLE_FIELDS.map do |f|
+      FieldDesc.new(self, reader_name: f,
+                    writer_name: true)
+    end
+
+    p = self.allocate
+
+    ## FIXME this may not gracefully handle some
+    ## formatting errors in the file.
+    ##
+    ## This assumes that the file can be parsed
+    ## as a top-level mapping or dictionary
 
     Psych.safe_load_file(file,
-                         symbolize_names: true).each do
+                         symbolize_names: true,
+                         aliases: true).each do
       |name,value|
-      fdesc = LOADABLE_DESCS.find  {
+      fdesc = descs.find  {
         |f| f.reader == name
       }
       if fdesc
@@ -56,30 +77,75 @@ class Proj
   attr_accessor :doc_files
   attr_reader   :extra_conf_data ## for serialization
 
-  ## NB still not enough as a field desc, each:
   LOADABLE_FIELDS = [:name, :version, :summary,
                      :description, :license, :lib_files,
-                     :doc_files]
-
-  LOADABLE_DESCS = LOADABLE_FIELDS.map do |f|
-    FieldDesc.new(self, reader_name: f,
-                  writer_name: true)
-  end
+                     :test_files, :doc_files]
 
 
   def set_extra_conf_data(name, value)
     if @extra_conf_data
       @extra_conf_data[name] = value
     else
-      @extra_conf_data = { name => value}
+      @extra_conf_data = { name => value }
     end
   end
+
+
+  def write_yaml_file(pathname, mode_flags: File::CREAT,
+                     **dumpargs)
+    ## FIXME store the original file pathname, during ::load_yaml
+    ##
+    ## & Write to that file, by default, here
+    ##
+    ## implies: Project Workspace
+    ##
+
+    f = File.open(pathname, mode_flags | File::RDWR)
+    write_yaml_stream(f, **dumpargs)
+    f.flush
+    return f
+    ensure
+      f.close
+  end
+
+  def write_yaml_stream(dest, **dumpargs)
+    dumpargs[:line_width] ||= 65
+    dumpargs[:header] ||= true
+
+    Psych.dump(self, dest, **dumpargs)
+  end
+
+
+  # def write_gemfile(dest)
+  # end
+
 end
+
+
+# END {
+#   ## was not reached
+#   puts "DEBUG: in END block for #{__FILE__}"
+# }
 
 
 =begin
 
-$FROB_P = Proj.load_yaml('../../rbloader.yprj')
+$FROB_P1 = Proj.load_yaml('../../rbloader.yprj')
 ## ^ simple enough on the input side ...
+
+$FROB_S00 = Psych.dump($FROB_P1, line_width: 65, header: true)
+
+$FROB_P2 = Psych.safe_load($FROB_S00, symbolize_names: true)
+
+## a general check for similarity across
+## different encoding/decoding calls
+
+IOST = StringIO.new
+
+$FROB_S01 = $FROB_P1.write_yaml_stream(IOST)
+
+IOST.pos=0
+
+$FROB_P3 = Psych.load_stream(IOST, symbolize_names: true)
 
 =end
