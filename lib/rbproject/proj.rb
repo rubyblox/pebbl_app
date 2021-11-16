e4ab20bba1737f9e31707012f8663ceca9add741
Sean Champ <spchamp@users.noreply.github.com>
2021-11-15 18:23:27 -0800

    [rbproject split] Checking in proj.rb version 0.2

--- lib/rbproject/proj.rb
+++ lib/rbproject/proj.rb
@@ -10,6 +10,9 @@ class FieldDesc
   def initialize(in_class, reader_name: nil,
                  writer_name: nil)
     @in_class = in_class
+
+    ## FIXME cannot call 'send' on actual method objects
+
     @reader = (reader_name &&
                in_class.instance_method(reader_name))
     if (writer_name == true) && reader_name
@@ -26,33 +29,18 @@ end
 class Proj
 
   def self.load_yaml(file)
-    ydoc = Psych.parse_file(file)
-    yvisitor = Psych::Visitors::ToRuby.create()
 
-    ## FIXME no type checking - assuming ydoc.children[0]
-    ## is of a type Psych::Nodes::Mapping
-    root_mapping = ydoc.children.pop
-    ## ^ FIXME pick up any remaining ydoc.children => ruby as extra data (??)
-    root_fields = root_mapping.children
     p = Proj.allocate
 
-    while ! ( root_fields.length.zero? )
-      data = root_fields.pop(2)
-      name = data[0].value.to_sym ## FIXME assuming scalar
-
-      puts ("read #{name}")
-
-      ## FIXME err if root_fields is empty at now:
-      ## FIXME value translation
-      ## FIXME fails on 'sequence'
-      value = yvisitor.send(:deserialize, data[1]) ## FIXME may err ..
-
+    Psych.safe_load_file(file,
+                         symbolize_names: true).each do
+      |name,value|
       fdesc = LOADABLE_DESCS.find  {
-        |f| f.reader == name
+        |f| f.reader.name == name
       }
       if fdesc
         ( writer = fdesc.writer ) &&
-          p.call(writer,value)
+          p.send(writer,value)
       else
         p.set_extra_conf_data(name,value)
       end
