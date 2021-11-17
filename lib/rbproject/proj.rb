d1e80a4787db14a36ff4ade1becc45e38162a912
Sean Champ <spchamp@users.noreply.github.com>
2021-11-16 17:50:23 -0800

    [rbproject split] rbproject proj.rb: Updating to 0.4.3
    - Adding preliminary YAMLInclude class
    
    - Adding a fields_from_conf field (r/w field)
    
    - Proj::DUMP_TAG renamed to YAML_TAG
    
    - Proj::YAML_TAG constant setter will now initialize
      both Psych.load_tags and Psych.dump_tags for the
      Proj class
    
    - Defining Proj::load_yaml_file in lieu of earlier
      Proj::load_yaml, with updated method now dispatching
      to Proj#load_yaml_stream
    
    - Defining Proj#load_yaml_field with contents
      of earlier Proj::load_yaml dispatching forms for
      Proj field init; Forms updated for preliminary
      YAMLInclude handling and updating @fields_from_conf
    
    - Updating for UTF-8 BOM handling in file-based
      load/write methods

--- lib/rbproject/proj.rb
+++ lib/rbproject/proj.rb
@@ -25,12 +25,62 @@ class FieldDesc
 end
 
 
+class YAMLInclude
+  attr_accessor :yaml_filename
+  attr_accessor :host_filename
+
+  VERSION= "0.0.1"
+
+  YAML_TAG= ( tag = "#{self.name}@#{self::VERSION}".freeze;
+             Psych.load_tags[tag]  = self;
+             Psych.dump_tags[self] = tag )
+
+  DIRECTIVE= "Include".to_sym
+
+  def coder_value(coder)
+    if (coder.type == :scalar)
+      return coder.scalar
+    else
+      ## FIXME use a more exacting exception type here
+      raise "Unsupported coder type #{coder.type} in #{coder}"
+    end
+  end
+
+  def init_with(coder)
+    ## NB usage under Psych::**::ToRuby
+    value = coder_value(coder)
+    if (value == DIRECTIVE)
+      ## NB the filename part of the mapping is what to load
+    else
+      ## TBD ensure that the filename will be processed for
+      ## adding subsequent mapping entries, Proj#load_yaml_stream
+    end
+  end
+
+  def encode_with(coder)
+    ## NB usage under Psych::**::YAMLTree
+    value = coder_value(coder)
+    ## TBD dump a tagged value ..
+    if (value == DIRECTIVE)
+      ## NB the filename part of the mapping is one of half of what to encode
+    else
+    end
+  end
+end
+
+
+
 ##
 ## Project class
 ##
 class Proj
 
-  VERSION="0.4.2"
+  ## TBD may have to use encode_with (if not also init_with) in
+  ## this class itself, to use some specific encode_with
+  ## semantics under possible configuration - incl. configuration
+  ## with "Include"
+
+  VERSION="0.4.3"
 
   ## FIXME when the setter for Psych.dump_tags[Proj]
   ## is placed instead in an END block in this file
@@ -39,45 +89,36 @@ class Proj
   ##
   ## Is something translating the END blocks to
   ## at_exit blocks for load under irb?
-  DUMP_TAG= Psych.dump_tags[self] ||=
-            "#{self.name}@#{self::VERSION}"
+  # YAML_TAG= Psych.dump_tags[self] ||=
+  #           "#{self.name}@#{self::VERSION}"
+          ## ^ FIXME not similarly: Psych::Psych.load_tags
 
-  def self.load_yaml(file, **loadopts)
-
-    descs = LOADABLE_FIELDS.map do |f|
-      FieldDesc.new(self, reader_name: f,
-                    writer_name: true)
-    end
-
-    p = self.allocate
+  YAML_TAG= ( tag = "#{self.name}@#{self::VERSION}".freeze;
+             Psych.load_tags[tag]  = self;
+             Psych.dump_tags[self] = tag )
 
     ## FIXME this may not gracefully handle some
     ## formatting errors in the file.
     ##
     ## This assumes that the file can be parsed
     ## as a top-level mapping or dictionary
+
+  def self.load_yaml_file(pathname, **loadopts)
+    ## FIXME may not be immediately useful
+    ## for deserialization to any subclass instance
     ##
-    ## FIXME this dictionary syntax does not
-    ## allow for project file includes, e.g
-    ## to reuse common project fields, in this
-    ## "YAML is not msbuild XML" hack
-
-    loadopts[:symbolize_names] ||= true
-    loadopts[:aliases] ||= true
-
-    Psych.safe_load_file(file, **loadopts).each do
-      |name,value|
-      fdesc = descs.find  {
-        |f| f.reader == name
-      }
-      if fdesc
-        ( writer = fdesc.writer ) &&
-          p.send(writer,value)
-      else
-        p.set_extra_conf_data(name,value)
-      end
-    end
-    return p
+    ## FIXME update to provide a 'project_class' arg,
+    ## here defaulting to to 'self', pursuant
+    ## of a usable demonstration of a subclass
+    ## of Proj
+    f = File.open(pathname, "rb:BOM|UTF-8")
+    instance = self.allocate
+    opts = loadopts.dup
+    opts[:filename] ||= pathname
+    instance.load_yaml_stream(f,**opts)
+    return instance
+    ensure
+      f.close
   end
 
   attr_accessor :name
@@ -85,10 +126,11 @@ class Proj
   attr_accessor :summary
   attr_accessor :description
   attr_accessor :license
-  attr_accessor :lib_files
-  attr_accessor :test_files
-  attr_accessor :doc_files
-  attr_reader   :extra_conf_data ## for serialization
+  attr_accessor :lib_files ## FIXME needs a sequence reader => add... under *from_yaml_stream
+  attr_accessor :test_files ## FIXME similar (previous)
+  attr_accessor :doc_files ## FIXME similar (previous)
+  attr_reader	:extra_conf_data ## for deserialization - hash table or nil
+  attr_accessor :fields_from_conf ## for debug
 
 
   ## FIXME This class needs additional fields, for broader gemspec
@@ -96,7 +138,7 @@ class Proj
   ## Specification Reference @
   ## https://guides.rubygems.org/specification-reference/
 
-  LOADABLE_FIELDS = [:name, :version, :summary,
+  SERIALIZE_FIELDS = [:name, :version, :summary,
                      :description, :license, :lib_files,
                      :test_files, :doc_files]
 
@@ -109,18 +151,116 @@ class Proj
     end
   end
 
+  def load_yaml_field(name, value, field_descs, source_file = nil)
 
-  def write_yaml_file(pathname, mode_flags: File::CREAT,
-                     **dumpargs)
+    if (name.instance_of?(YAMLInclude))
+      yfile = value.yaml_source_file
+      abs = File.expand_path(yfile, source_file && File.dirname(source_file))
+      @fields_from_conf.push [abs, source_file]
+      ## FIXME provide an "load option" to continue
+      ## when the include file is not available
+      self.load_yaml_file(abs,**loadopts)
+    else
+      @fields_from_conf.push [name, source_file]
+
+      fdesc = @fdescs.find  {
+        |f| f.reader == name
+      }
+      ## FIXME parse for a name == :include
+      ## using the value as a source_file relative to this file
+      ## subsq initializing 'instance' with any data from
+      ## that file (or failing if inaccessible)
+
+      ## FIXME reimplement onto Psych::Coder ??
+      if (fdesc && ( writer = fdesc.writer ))
+        self.send(writer,value)
+      elsif (name == :extra_conf_data)
+        ## assumption: the value represents a hash table, such
+        ## that would have been encoded as a YAML mapping
+        value.each do |exname, exvalue|
+          self.set_extra_conf_data(exname,exvalue)
+        end
+      else
+        ## unkonwn top level { name => value } pair
+        ## will be stored in extra_conf_data
+        self.set_extra_conf_data(name,value)
+      end
+    end
+  end
+
+
+  def load_yaml_file(filename, **loadopts)
+  end
+
+  def load_yaml_stream(io, **loadopts)
+
+    ## FIXME FDESCS should be stored and accessed
+    ## via a class method
+    @fdescs ||= self.class::SERIALIZE_FIELDS.map do |f|
+      ## populate @fdescs with an array of
+      ## FieldDesc objects for this class
+      FieldDesc.new(self, reader_name: f,
+                    writer_name: true)
+    end
+
+    ## FIXME this may not gracefully handle some
+    ## formatting errors in the file.
     ##
-    ## FIXME store the original file pathname, during ::load_yaml
+    ## This assumes that the file can be parsed
+    ## as a top-level mapping or dictionary
+
+    ## FIXME this dictionary syntax does not
+    ## allow for project file includes, e.g
+    ## to reuse common project fields, in this
+    ## "YAML is not msbuild XML" hack
+
+    opts = loadopts.dup
+
+    opts[:symbolize_names] ||= true
+    opts[:aliases] ||= true
+
+    ## NB debug storage - FIXME will not overwrite previous
+    ## entries in @fields_from_conf if the instance is initialized
+    ## more than once, from the same file & includes. That should
+    ## be managed in some calling method, to set @fields_from_conf
+    ## to an empty array before reinitializing the instance from
+    ## configuration files
+    @fields_from_conf ||= []
+    if ( filename = opts[:filename] )
+      filename = File.expand_path(filename)
+    end
+
+    Psych.safe_load(io, **opts).each do
+      ## initialize the new instance 'instance'
+      ## per { key => value } pairs in the data
+      ## deserialized from the top-level mapping
+      ## assumed to have been encoded in the YAML
+      ## stream
+      ##
+      ## Assumptions:
+      ## - the value of <name> is a symbol
+      ## - the value of <value> is a list, hash,
+      ##   or scalar value
+      |name, value|
+      load_yaml_field(name, value, @fdescs, filename)
+    end
+    return self
+  end
+
+  def write_yaml_file(pathname, **dumpargs)
+    ##
+    ## FIXME store the original file pathname, during ::load_yaml_file
     ##
     ## & Write to that file by default, here
     ##
     ## implies: Project Workspace
     ##
 
-    f = File.open(pathname, mode_flags | File::RDWR)
+    ## FIXME if @fields_from_conf is non-nil and non-empty
+    ## then iterate across the same,
+    ## else ... TBD/FIXME using SERIALIZE_FIELDS
+
+    f = File.open(pathname, "wb:BOM|UTF-8")
     write_yaml_stream(f, **dumpargs)
     f.flush
     return f
@@ -156,7 +296,7 @@ end
 ## trivial tests for Proj serialization/deserialization
 ## onto YAML
 
-$FROB_P1 = Proj.load_yaml('../../rbloader.yprj')
+$FROB_P1 = Proj.load_yaml_file('../../rbloader.yprj')
 
 $FROB_S00 = Psych.dump($FROB_P1, line_width: 65, header: true)
 
