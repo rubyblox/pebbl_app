702edd420e955028a4355bc6bf6079e1baeb5989
Sean Champ <spchamp@users.noreply.github.com>
2021-12-19 21:57:19 -0800

    ProjectKit: Updating module definition

--- projectkit/lib/projectkit.rb
+++ projectkit/lib/projectkit.rb
@@ -1,10 +1,10 @@
 ## projectkit.rb - baseline module definition and autoloads
 
 module ProjectKit
-  SOURCEDIR=File.join(__dir__, self.name.downcase)
+  SOURCEDIR=File.join(__dir__, self.name.downcase).freeze
 
-  VERSION=File.expand_path("projectkit_version.inc", SOURCEDIR).
-            split("\n").grep(/^[[:space:]]*[^#]/).first
+  VERSION=File.read(File.join(SOURCEDIR, self.name.downcase + "_version.inc")).
+                      split("\n").grep(/^[[:space:]]*[^#]/).first.freeze
 
   AUTOLOAD_CLASSES=%w(RSpecTool)
 
@@ -12,3 +12,7 @@ module ProjectKit
     autoload(name, File.expand_path(name.downcase + ".rb", SOURCEDIR))
   }
 end
+
+# Local Variables:
+# fill-column: 65
+# End:
