effe332107e1ff9903027a16263463de4b56e641
Sean Champ <spchamp@users.noreply.github.com>
2021-12-19 17:21:22 -0800

    project_tools (RSpecTool) : Updating for pathname handling
    RSpecTool.fchdir
    - New method. Fork & call block, exit with or without error
    
    RSpecTool.find_project_root
    - New method
    
    RSpecTool.project_libdir
    - New method
    
    RSpecTool.project_testdir
    - New method
    
    RSpecTool.find_rspec_for
    - New method
    
    RSpecTool.run
    - Updating to compute a default basedir via find_project_root()
    - Updating to ensure project_libdir() is used for computing library
      paths
    - Updating to use fchdir
    
    RSpecTool class
    - Adding constants

--- gemreg/lib/project_tools.rb
+++ gemreg/lib/project_tools.rb
@@ -1,33 +1,145 @@
 ## project_tools.rb - utility methods (non-gem source file)
 
+require 'pathname'
+
 class RSpecTool
-  def self.run(basedir, tests, args: %w[--no-color],
+
+  RSPEC_LOCAL_SUFFIX=".rspec"
+  RSPEC_PROJECT_SUFFIX=".rb"
+  PROJECT_LIBDIR_RELATIVE="lib"
+  PROJECT_TESTDIR_RELATIVE="test"
+  PROJECT_ROOT_FILES=%w(*.gemspec *.yproj Rakefile .git) ## NB name | case-insensitive glob patterns
+
+  RSPEC_DEFAULT_ARGS=%w(--no-color)
+
+  def self.fchdir(dir, *block_args, &block)
+    ## NB this of course requires a usable fork() implementation on
+    ## the host
+    ##
+    ## NB the block may call 'exit' before either of the
+    ## handlers, below, would be reached
+    ##
+    unless (has_block = block_given?)
+      warn("No block provided to #{self}.#{__method__}", uplevel: 1)
+    end
+
+    pid = Process.fork()
+    if pid
+      pid, status = Process.waitpid2(pid)
+      return status.exitstatus
+    else
+      begin
+        Dir.chdir(dir)
+        block.yield(*block_args) if has_block
+      rescue
+        Kernel.exit(-1)
+      else
+        Kernel.exit(0)
+      end
+    end
+  end
+
+  def self.find_project_root(whence = Dir.pwd)
+    ## NB this recognizes only a single-character File::SEPARATOR
+    whence_dir_p = File.exists?(whence) ? File.directory?(whence) :
+                     ((whence.length.eql?(0)) ? true :
+                        (whence[-1] == File::SEPARATOR))
+    base = File.expand_path(whence_dir_p ? whence : File.dirname(whence))
+    fnflags = (File::FNM_DOTMATCH | File::FNM_SYSCASE | File::FNM_CASEFOLD)
+    path = nil
+    catch(:found) {
+      Dir.children(base).each { |chfile|
+        PROJECT_ROOT_FILES.each { |glob|
+          if File.fnmatch(glob, chfile, fnflags)
+            path = base
+            throw(:found)
+          end
+        }
+      }
+    }
+    if path
+      return path
+    else
+      p = Pathname.new(base)
+      if p.root?
+        return nil
+      else
+        find_project_root(p.parent.to_s)
+      end
+    end
+  end
+
+
+  def self.project_libdir(whence)
+    root = find_project_root(whence)
+    dir = File.expand_path(PROJECT_LIBDIR_RELATIVE, root)
+    dir if File.exists?(dir)
+  end
+
+
+  def self.project_testdir(whence)
+    root = find_project_root(whence)
+    dir = File.expand_path(PROJECT_TESTDIR_RELATIVE, root)
+    dir if File.exists?(dir)
+  end
+
+
+
+  def self.find_rspec_for(file)
+    f = file.to_s
+    extlen = File.extname(f).length
+    if (extlen < f.length)
+      name = f[...-extlen]
+    else
+      name = f
+    end
+
+    rspec_local = name + RSPEC_LOCAL_SUFFIX
+    if File.exists?(rspec_local)
+      return rspec_local
+    else
+      root = find_project_root(f) ## FIXME
+      return nil unless root
+      lib_root = project_libdir(root)
+      test_root = project_testdir(root)
+      p = Pathname.new(File.expand_path(f))
+      p_rel = p.relative_path_from(lib_root).to_s
+      name_rel = p_rel[...-extlen]
+      proj_rspec =
+        File.join(test_root, name_rel + RSPEC_PROJECT_SUFFIX)
+      if File.exists?(proj_rspec)
+        return proj_rspec
+      else
+        return nil
+      end
+    end
+  end
+
+  def self.run(tests,
+               basedir: find_project_root(),
+               args: RSPEC_DEFAULT_ARGS,
                stderr: STDERR, stdout: STDOUT)
     ## NB this method does not provide any support for host environments
     ## that provide no usable fork() implementation
     dir = File.expand_path(basedir)
-    paths = tests.map { |f| File.expand_path(File.dirname(f)) }.sort.uniq
+    paths = tests.map { |f| project_libdir(f) }.sort.uniq
     use_args = args.dup.concat(tests)
 
-    pid = Process.fork{
-      Dir.chdir(dir)
+
+    fproc = lambda {
       $LOAD_PATH.concat(paths)
       gem 'rspec'
       gem 'rspec-core'
       require 'rspec/core'
-      whence = ::RSpec::Core::Runner
-      whence.disable_autorun!
-      st = whence.run(use_args, stderr, stdout).to_i
-      exit(st)
+      obj = ::RSpec::Core::Runner
+      obj.disable_autorun!
+      st = obj.run(use_args, stderr, stdout).to_i
+      Kernel.exit(st)
+      ## TBD assuming no interverning errors (e.g in exit after
+      ## forking irb), test the process exit code, via the status
+      ## object that will be returned in the forking process
     }
-    if block_given?
-      ## NB the block should call Proceses.wait or Process.waitpid on
-      ## the PID
-      yield pid
-    else
-      st = Process.waitpid2 pid
-      return st[1]
-    end
+    self.fchdir(dir, &fproc)
   end
 end
 
