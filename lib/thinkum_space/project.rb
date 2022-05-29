4ea29716223aece851e703f3400066b032332479
Sean Champ <spchamp@users.noreply.github.com>
2022-05-29 15:26:23 -0700

    ThinkumSpace::Project: New project
    Files were created with the following shell command, within local shell
    scripting for git stash and source-file merge with the present rblib
    work tree:
    ~~~~
    $ bundle gem --no-changelog --no-mit --no-coc --test=rspec \
           --ci=github --linter=standard thinkum_space-project
    ~~~~
    
    The gemspec name thinkum_space-project has been interpolated by
    bundle-gem(1) in the creation of a module ThinkumSpace::Project.
    
    Supporting files in this changeset were created by bundle-gem(1)
    
    bundle-gem(1) also added the files for commit to the existing Git
    repository then at the directory ./thinkum_space-project/. This
    directory was a copy of the original rblib work tree, cleared of
    any existing changes and any unstaged files using git-stash(1). Any
    changes or unstaged files would have been quietly added by bundle-gem(1)
    
    The ThinkumSpace::Project project should be the destination of earlier
    rblib projectutils/projectkit sources including project_tools.rb
    
    After merge of any earlier gemspec definitions to the root project
    directory for this project and addition of each updated gemspec under a
    gemspec 'name' reference in the root Gemfile, the merged ThinkumSpace::Project
    gem may then be accessible for development purposes from those updated
    gemspecs.
    
    Presently, it is not expected that the ThinkumSpace::Project gem will be
    distributed for any out-of-tree applications. The gem has been created
    to serve as a container for project-related tooling within this working
    source tree, integrating with Ruby gem(1), bundler(1) and other tools
    available for Ruby programming.

---
+++ lib/thinkum_space/project.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "project/version"
+
+module ThinkumSpace
+  module Project
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/thinkum_space/project/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ThinkumSpace
+  module Project
+    VERSION = "0.1.0"
+  end
+end
