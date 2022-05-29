021a9943c7dbfa7dc13ad5892767a610b5c0a627
Sean Champ <spchamp@users.noreply.github.com>
2022-05-28 21:00:13 -0700

    apputils: Updating old testing code; Adding Gemfile, gemspec

---
+++ rbproject/apputils/apputils.gemspec
@@ -0,0 +1,13 @@
+## apputils gemspec
+
+Gem::Specification.new do |s|
+  s.name = 'apputils'
+  s.version =  '0.0.1'
+  s.licenses = ['MPL-2.0']
+  s.summary = 'Application utilities'
+  s.description = s.summary + ' from Thinkum.Space'
+  s.authors = ['Sean Champ']
+  s.email = 'spchamp@users.noreply.github.com'
+  s.files = ['lib/apputils/assochash.rb',
+             'lib/apputils/options.rb']
+end
--- rbproject/apputils/lib/apputils/options-tests.rb
+++ rbproject/apputils/lib/apputils/options-tests.rb
@@ -1,9 +1,9 @@
-## options-test.rb - simple tests for options.rb
+## options-test.rb - ad hoc tests for options.rb
 
-require('consolekit/options') ## the library to test
+require('apputils/options') ## the library to test
 
 
-om = OptionMap.new({:a => 1, :b => true })
+om = OptionMap::OptionMap.new({:a => 1, :b => true })
 
 om.getopt(:a) == 1
 
@@ -12,11 +12,10 @@ om.getopt(:b) == true
 om.getopt(:c) == false
 
 
-on = OptionMap.new([:c, :d])
+on = OptionMap::OptionMap.new([:c, :d])
 
 on.getopt(:c) == true
 
 on.getopt(:d) == true
 
 on.getopt(:a) == false
-
