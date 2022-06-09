e3675e3ce9515c8852b5112748249ee3479fb0fc
Sean Champ <spchamp@users.noreply.github.com>
2022-06-09 00:35:37 -0700

    GApp::Support::AppModule spec: Adding tests for data_dirs, config_dirs methods

--- spec/g_app/support/app_module_spec.rb
+++ spec/g_app/support/app_module_spec.rb
@@ -4,7 +4,7 @@
 require 'g_app/support/app_module'
 
 
-shared_examples "configured by process environment" do |conf|
+shared_examples "scalar configured by process environment" do |conf|
   let!(:var) { conf[:var] }
   let!(:initial_value) { ENV[var] }
   let(:value) { conf[:value] }
@@ -20,31 +20,63 @@ shared_examples "configured by process environment" do |conf|
   end
 end
 
+shared_examples "path array configured by process environment" do |conf|
+  let!(:var) { conf[:var] }
+  let!(:initial_value) { ENV[var] }
+  let(:value) { conf[:value] }
+  let(:mtd) { conf[:mtd] }
+
+  it "to configure #{described_class}.#{conf[:mtd]} from #{conf[:var]}" do
+    ENV[var] = value
+    expect(subject.send(mtd)).to be == value.split(File::PATH_SEPARATOR)
+  end
+
+  after(:each) do
+    ENV[var] = initial_value
+  end
+end
+
 
 describe GApp::Support::AppModule do
-  it_behaves_like "configured by process environment",
+
+  it_behaves_like "scalar configured by process environment",
     mtd: :data_home, var: described_class::Const::XDG_DATA_HOME_ENV,
     value: "dirs/data"
 
-  it_behaves_like "configured by process environment",
+  it_behaves_like "scalar configured by process environment",
     mtd: :config_home, var: described_class::Const::XDG_CONFIG_HOME_ENV,
     value: "dirs/config"
 
-  it_behaves_like "configured by process environment",
+  it_behaves_like "scalar configured by process environment",
     mtd: :cache_home, var: described_class::Const::XDG_CACHE_HOME_ENV,
     value: "dirs/cache"
 
-  it_behaves_like "configured by process environment",
+  it_behaves_like "scalar configured by process environment",
     mtd: :state_home, var: described_class::Const::XDG_STATE_HOME_ENV,
     value: "dirs/state"
 
-  it_behaves_like "configured by process environment",
+  it_behaves_like "scalar configured by process environment",
     mtd: :tmpdir, var: described_class::Const::TMPDIR_ENV,
     value: "dirs/tmp"
 
-  it_behaves_like "configured by process environment",
+  it_behaves_like "scalar configured by process environment",
     mtd: :home, var: described_class::Const::HOME_ENV,
     value: "dirs/home"
 
+  ## test path list of more than one element (data dirs, config dirs)
+  it_behaves_like "path array configured by process environment",
+    mtd: :data_dirs, var: described_class::Const::XDG_DATA_DIRS_ENV,
+    value: "dirs/data:usr/dirs/data"
+  it_behaves_like "path array configured by process environment",
+    mtd: :config_dirs, var: described_class::Const::XDG_CONFIG_DIRS_ENV,
+    value: "dirs/config:usr/dirs/config"
+
+  ## test path list of one element (data dirs, config dirs)
+  it_behaves_like "path array configured by process environment",
+    mtd: :data_dirs, var: described_class::Const::XDG_DATA_DIRS_ENV,
+    value: "dirs/data"
+  it_behaves_like "path array configured by process environment",
+    mtd: :config_dirs, var: described_class::Const::XDG_CONFIG_DIRS_ENV,
+    value: "dirs/config"
 
 end
