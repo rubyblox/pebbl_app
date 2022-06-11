0a91d2f881a5238ec6e9c18b3531078bec43e051
Sean Champ <spchamp@users.noreply.github.com>
2022-06-11 15:50:23 -0700

    reinding source text in rspec tests for PebblApp::GtkSupport::GtkConfig
    PebblApp::GtkSupport::GtkConfig: Reindenting source text after
    refactoring for config

--- spec/pebbl_app/gtk_support/gtk_config_spec.rb
+++ spec/pebbl_app/gtk_support/gtk_config_spec.rb
@@ -5,81 +5,79 @@ require 'pebbl_app/gtk_support/gtk_config'
 
 describe PebblApp::GtkSupport::GtkConfig do
 
-#    context "GTK support" do
-      ## tests specs defined here may modify DISPLAY in the test
-      ## environment. The initial value should be restored after
-      ## each test.
-      ##
-      ## This text context requires that some DISPLAY is available.
-      ## See also: Xvfb(1)
-      let!(:display_initial) { ENV['DISPLAY'] }
+  ## tests specs defined here may modify DISPLAY in the test
+  ## environment. The initial value should be restored after
+  ## each test.
+  ##
+  ## This text context requires that some DISPLAY is available.
+  ## See also: Xvfb(1)
+  let!(:display_initial) { ENV['DISPLAY'] }
 
-      before(:each) do
-        subject.parsed_args=[]
-        subject.unset_display
-        subject[:debug] = true
-      end
+  before(:each) do
+    subject.parsed_args=[]
+    subject.unset_display
+    # subject[:debug] = true ## FIXME not used now
+  end
 
-      after(:each) do
-        ENV['DISPLAY'] = display_initial
-      end
+  after(:each) do
+    ENV['DISPLAY'] = display_initial
+  end
 
-      it "Uses a GtkConfig for config" do
-        expect(subject).to be_a PebblApp::GtkSupport::GtkConfig
-      end
+  it "Uses a GtkConfig for config" do
+    expect(subject).to be_a PebblApp::GtkSupport::GtkConfig
+  end
 
-      it "parses arg --display DPY" do
-        subject.parse_opts(%w(--display :10))
-        expect(subject.display).to be == ":10"
-      end
+  it "parses arg --display DPY" do
+    subject.parse_opts(%w(--display :10))
+    expect(subject.display).to be == ":10"
+  end
 
-      it "parses arg --display DPY to gtk_args" do
-        subject.parse_opts(%w(--display :10))
-        expect(subject.gtk_args).to be == %w(--display :10)
-      end
+  it "parses arg --display DPY to gtk_args" do
+    subject.parse_opts(%w(--display :10))
+    expect(subject.gtk_args).to be == %w(--display :10)
+  end
 
-      it "parses arg --display=DPY" do
-        subject.parse_opts(%w(--display=:11))
-        expect(subject.display).to be == ":11"
-      end
+  it "parses arg --display=DPY" do
+    subject.parse_opts(%w(--display=:11))
+    expect(subject.display).to be == ":11"
+  end
 
-      it "parses arg --display=DPY to gtk_args" do
-        subject.parse_opts(%w(--display=:11))
-        expect(subject.gtk_args).to be == %w(--display :11)
-      end
+  it "parses arg --display=DPY to gtk_args" do
+    subject.parse_opts(%w(--display=:11))
+    expect(subject.gtk_args).to be == %w(--display :11)
+  end
 
-      it "parses arg -dDPY" do
-        subject.parse_opts(%w(-d:12))
-        expect(subject.display).to be == ":12"
-      end
+  it "parses arg -dDPY" do
+    subject.parse_opts(%w(-d:12))
+    expect(subject.display).to be == ":12"
+  end
 
-      it "parses arg -dDPY to gtk_args" do
-        subject.parse_opts(%w(-d:12))
-        expect(subject.gtk_args).to be == %w(--display :12)
-      end
+  it "parses arg -dDPY to gtk_args" do
+    subject.parse_opts(%w(-d:12))
+    expect(subject.gtk_args).to be == %w(--display :12)
+  end
 
-      it "resets parsed args" do
-        subject.parsed_args=[]
-        subject.configure(argv: [])
-        expect(subject.parsed_args).to be == []
-      end
+  it "resets parsed args" do
+    subject.parsed_args=[]
+    subject.configure(argv: [])
+    expect(subject.parsed_args).to be == []
+  end
 
-      it "indicates when no display is configured" do
-        ENV.delete('DISPLAY')
-        null_argv = []
-        subject.configure(argv: null_argv)
-        expect(subject.display?).to_not be_truthy
-      end
+  it "indicates when no display is configured" do
+    ENV.delete('DISPLAY')
+    null_argv = []
+    subject.configure(argv: null_argv)
+    expect(subject.display?).to_not be_truthy
+  end
 
-      it "overrides env DISPLAY with any display arg" do
-        if (initial_dpy = ENV['DISPLAY'])
-          ENV['DISPLAY']= initial_dpy + ".nonexistent"
-          subject.parse_opts(['--display', initial_dpy])
-          expect(subject.display).to be == initial_dpy
-        else
-          RSpec::Expectations.fail_with("No DISPLAY configured in test environment")
-        end
-      end
+  it "overrides env DISPLAY with any display arg" do
+    if (initial_dpy = ENV['DISPLAY'])
+      ENV['DISPLAY']= initial_dpy + ".nonexistent"
+      subject.parse_opts(['--display', initial_dpy])
+      expect(subject.display).to be == initial_dpy
+    else
+      RSpec::Expectations.fail_with("No DISPLAY configured in test environment")
+    end
+  end
 
-#    end
 end
