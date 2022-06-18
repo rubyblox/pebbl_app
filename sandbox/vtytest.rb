df168454d84d4e7066ecacb0112c20143310b397
Sean Champ <spchamp@users.noreply.github.com>
2022-06-17 17:09:46 -0700

    vtytest: adding prefs window, updating app/window init; shell config change
    vtytest.rb
    - renaming local G%Proto modules => %Proto
    - disambiguating scope for calls to GLib/Gtk type init methods within
      %Proto module methods
    - VtyPrefsWindow: new class
    - VtyAppWindow, VtyApp:
      - adding additional signal handler methods
      - implementing some stub methods, dispatched under signal handling
      - removing earlier action definitions in UI / action bindings in *.rb
    - VtyApp
      - moving default_shell accessor to the appliation instance scope
      - removing #guess_active_window - preferring Gtk::Application#active_window
      - adding application flags to the call for #initialize with superclass
        method (send environment, non unique)
      - adding accessors for using at most one prefs window, methods for
        prefs window initialization and cleanup
      - adding create_app_window, dispatched from the updated handle_activate
        for VtyApp
    
    appwindow.vtytest.ui
    - removing action bindings
    - adding signal handler bindings
    - adding entry buff, completion for vty_entry widget
    
    prefs.vtytest.ui
    - new file, UI implemented in the new VtyPrefsWindow class

--- sandbox/vtytest.rb
+++ sandbox/vtytest.rb
@@ -7,39 +7,43 @@ require 'vte3'
 require 'shellwords'
 
 
-module GBoxedProto
+module BoxedProto
   def self.extended(whence)
-    if ! class_variable_defined?(:@@registered)
+    if ! whence.class_variable_defined?(:@@registered)
       ## FIXME does not detect duplcate registrations
       ## of differing implmentation classes
       whence.type_register
-      @@registered = true
+      whence.class_variable_set(:@@registered, true)
     end
   end
 end
 
-module GtkCompositeProto
+module CompositeProto
   def self.extended(whence)
-    whence.extend GBoxedProto
+    whence.extend BoxedProto
 
-    ## common method for file-based or resource-path-based composite classes
+    ## common method for init with file-based or resource-path-based
+    ## composite classes
     def initialize_template_children
       self.template_children.each do |id|
         Kernel.warn("Binding template child #{id} for #{self}", uplevel: 0) ## DEBUG
         bind_template_child(id)
       end
 
-      set_connect_func do |name|
+      self.set_connect_func do |name|
         Kernel.warn("Binding method #{name} for #{self}", uplevel: 0) ## DEBUG
         method name
       end
     end
-  end
+  end ## self.extended
 end
 
-module GtkCompositeFileProto
+## prototype module for classes deriving a Gtk Builder template
+## definition directly from a UI file source
+##
+module CompositeFileProto
   def self.extended(whence)
-    whence.extend GtkCompositeProto
+    whence.extend CompositeProto
 
     ## initialize a file-based template for this class, using the class
     ## constant TEMPLATE to determine the template file's pathname
@@ -54,7 +58,7 @@ module GtkCompositeFileProto
           fio = gfile.read
           nbytes = File.size(self::TEMPLATE)
           bytes = fio.read_bytes(nbytes)
-          Kernel.warn("Setting template for #{self} from #{self::TEMPLATE}",
+          Kernel.warn("Setting template data for #{self} @ #{self::TEMPLATE}",
                       uplevel: 0)
           self.set_template(data: bytes)
         ensure
@@ -72,9 +76,15 @@ module GtkCompositeFileProto
 end
 
 
+## configuration support for VtyApp
+##
+## an instance of this class is returned from VtyApp#config. The
+## instance will have been initialized for the application command name
+## of the calling VtyApp
+##
 class VtyConfig < PebblApp::GtkSupport::GtkConfig
 
-  ## configuration support for VtyApp - command line options paring
+  ## command line options parsing vor VtyApp
   def configure_option_parser(parser)
     super(parser)
     parser.on("-c", "--command COMMAND", "Command to use for shell") do |cmd|
@@ -85,19 +95,82 @@ class VtyConfig < PebblApp::GtkSupport::GtkConfig
 end
 
 
-class VtyAppWindow < Gtk::ApplicationWindow
-  TEMPLATE ||=
-    File.expand_path("../ui/appwindow.vtytest.ui", __dir__)
 
-  extend GBoxedProto
-  extend GtkCompositeFileProto
+class VtyPrefsWindow < Gtk::Dialog
+  TEMPLATE ||=
+    File.expand_path("../ui/prefs.vtytest.ui", __dir__)
+
+  extend BoxedProto
+  extend CompositeFileProto
 
   class << self
     ## configuration for initialize_template_children in init
     def template_children
-      %w(vty vty_send vtwin_vty_menu vty_app_menu vty_menu)
+      %w(shell_cmd_entry io_model_combo prefs_stack prefs_sb)
     end
   end ## class << self
+
+
+  def initialize(**args)
+    super(**args)
+  end
+
+  ## unmap and recycle the window
+  def close
+    self.unmap
+    self.destroy
+  end
+
+  ##
+  ## -- signal receivers for the window --
+  ##
+  ## mapped to actionable widgets via the Glade UI editor, "signals" panel
+  ##
+
+  def vty_io_model_changed(obj)
+    ## mapped to the 'changed' signal in the io_model_combo widget
+  end
+
+  def vty_default_shell_changed(obj)
+    ## mapped to the 'changed' signal in the shell_cmd_entry widget
+  end
+
+
+  ## close the prefs window without updating configuration changes
+  def prefs_cancel(obj)
+    ## mapped to the "clicked" signal for a button in the dialog window
+    Kernel.warn("cancel @ #{obj} => #{self}", uplevel: 0)
+    self.close
+  end
+
+  ## apply configuration changes without closing the window
+  def prefs_apply(obj)
+    ## mapped to the "clicked" signal for a button in the dialog window
+  end
+
+  ## apply configuration configuration changes and close the window
+  def prefs_ok(obj)
+    ## mapped to the "clicked" signal for a button in the dialog window
+    self.close
+  end
+end
+
+
+class VtyAppWindow < Gtk::ApplicationWindow
+  TEMPLATE ||=
+    File.expand_path("../ui/appwindow.vtytest.ui", __dir__)
+
+  extend BoxedProto
+  extend CompositeFileProto
+
+  class << self
+    ## configuration for initialize_template_children in init
+    def template_children
+      %w(vty vty_send vtwin_vty_menu vty_app_menu vty_menu
+         vty_entry vty_entry_buff vty_entry_completion)
+    end
+  end ## class << self
+
 
   def initialize(app)
     Kernel.warn("Initializing #{self}", uplevel: 0)
@@ -105,36 +178,83 @@ class VtyAppWindow < Gtk::ApplicationWindow
     ## bind actions, signals ...
   end
 
+
   ###########################################3
-  ## signal handler methods
+  ## signal receiver methods
   ## - configured e.g via the 'signals' tab in the Glade UI editor
-  ## - the "handler" there may represents a method name,
+  ## - the "handler" there may represent a method name,
   ##   while the "user data" widget selected there may represent
   ##   the recipient of the method
+  ## - if receiver method is defined as name(obj) then the obj may represent
+  ##   the "active widget" of the signal's activation, e.g a menu item
+
+  def vtwin_close
+    self.close
+  end
 
   def vty_eof
-    ## self.vte ...
+    ## self.class.send_vte_eof(self.vte) ...
   end
 
   def vty_reset
-    ## self.vte ...
+    self.vty.reset(false, false)
+  end
+
+  def vtwin_show_prefs(obj)
+    ## ... map a prefs window, attached to the active window for this
+    ## window's application, if no prefs window already exists,
+    ## else raise the prefs window
+
+    Kernel.warn("Prefs method received data: #{obj.inspect}",
+                uplevel: 0)
+    app = self.application
+    app.prefs_window ||= app.create_prefs_window(app.active_window || self)
+    # if (! apps.prefs_window.visible?)
+    #   app.prefs_window.show
+    # end
+    # app.prefs_window.raise
+    ## or (??)
+    app.prefs_window.present
+  end
+
+  def vtwin_show_about(obj)
+  end
+
+  def vtwin_new
+    win = self.class.new(self.application)
+    win.show
+  end
+
+  def vtwin_save_text
+    ## self.class.save_vte_text(...)
+  end
+
+  def vtwin_save_data
+    ## self.class.save_vte_data(...)
+  end
+
+  def vtwin_send
+    ## from the 'send' button - send any text in vty_entry_buff
+    ## to the input stream of the pty/pipe
+  end
+
+  def vtwin_received_eof(vty)
+  end
+
+  def vtwin_received_exit(vty)
   end
 
 end
+
 
 class VtyApp < Gtk::Application
   include PebblApp::GtkSupport::GtkAppPrototype
 
   class << self
-    def default_shell()
-      if class_variable_defined?(:@@default_shell)
-        @@default_shell
-      else
-        Vte.user_shell
-      end
-    end
 
     def started
+      ## TBD state recording for instances of this app class
+      ## onto Gtk dbus conventions
       if class_variable_defined?(:@@started)
         @@started
       else
@@ -145,8 +265,22 @@ class VtyApp < Gtk::Application
     def started=(inst)
       @@started = inst
     end
-
   end ## class << self
+
+  def default_shell=(cmd)
+    @default_shell = cmd
+  end
+
+  def default_shell
+    ## TBD - should be a configurable property of the app and/or window
+    if instance_variable_defined?(:@default_shell)
+      return @default_shell
+    elsif self.config.option?(:shell)
+      return self.config.options[:shell]
+    else
+      return Vte.user_shell
+    end
+  end
 
 
   def action_group_name()
@@ -169,7 +303,7 @@ class VtyApp < Gtk::Application
   ## Glade UI editor, not menu actions per se
   def map_simple_action(name, group: nil,
                         accel: nil, &handler)
-
+    ## GSimpleAction is the name of a class
     if (! group)
       if (name.include?("."))
         elts = name.split(".")
@@ -203,47 +337,33 @@ class VtyApp < Gtk::Application
     end
   end
 
-  def guess_active_window()
-    windows = self.windows
-    if windows.empty?
-      raise "No active window"
-    else
-      ## FIXME trivial parse - the first window may not have been the
-      ## active window e.g for which a prefernces menu item was selected
-      ##
-      ## thus, the signal handlers approach would be preferred here,
-      ## notwithstanding particulars of rhetoric about GAction usage,
-      ## menus, and proposals for UI designs in the ostensible wiki docs
-      ##
-      ## - signal handlers can be mapped to actual objects. This is -
-      ##   for now - fairly easy to configure with the Glade UI editor
-      ##
-      ## if one must try out an approach with GAction ...
-      windows.first
-    end
-  end
-
-
   def register()
-    ## not reached unless called directly here
-    ## though a superclass #register method is reached via some other call
+    ## may not be reached in the underlying API when this method is
+    ## overridden, unless this method is called directly as from this
+    ## class or a subclass
     Kernel.warn("Registering #{self.inspect}", uplevel: 0)
-    ## fails from #start - duplicate registration (when? by what call?)
     super()
   end
+
+  ## TBD accessor for the 'active-window' property on this Gtk::Application
 
   def initialize()
     ## the first arg for the Gtk::Application constructor is required,
     ## and requires a specific syntax e.g
-    super("space.thinkum.vtytest")
-    ## ^ TBD does this call 'register' in some way ?
+    super("space.thinkum.vtytest",
+          Gio::ApplicationFlags::SEND_ENVIRONMENT |
+            Gio::ApplicationFlags::NON_UNIQUE)
+    ## ^ TBD does this call 'register' in some way, e.g in Gtk ?
+    ## ^ TBD calling with flags for multiple instance of the app
 
-    ## map a handler for the the 'startup' signal,
-    ## reached after self.register
+    ## map a handler for the app 'startup' signal,
+    ## reached e.g via self.register
     signal_connect "startup" do |app|
       app.handle_startup
     end
 
+    ## map a handler for the app 'activate' signal
+    ## might be reached via self.run
     signal_connect "activate" do |app|
       app.handle_activate
     end
@@ -267,39 +387,40 @@ class VtyApp < Gtk::Application
                   uplevel: 1)
     end
 
-    @running = self
 
-    ## map a GAction for 'app.quit' on this GtkApplication
-    #
-    # quit = Gio::SimpleAction.new("quit")
-    # quit.signal_connect("activate") do
-    #   application.quit
-    # end
-    # app.add_action(quit)
-    # app.set_accels_for_action("app.quit", quit)
-    ## or:
+    ## bind C-q to an 'app.quit' action,
+    ## also defining a handler for that action
     self.map_simple_action("app.quit", accel: "<Ctrl>Q") do
       self.quit
     end
-
-    self.map_simple_action('app.prefs') do # |action, param|
-      ## it would be great if the action initializer could pass an
-      ## activating window as the param, e.g via the 'activate' signal
-      ## on each menu item - whether or not this would be compatible
-      ## with the novelty of GVariant (it would not be)
-      ##
-      # prefs = VtyPrefs.new(:transient_for => self.guess_active_window), use_header_bar: true)i
-      ## alernately, there are singal handlers - such that will probably not
-      ## ever be removed from the GTK API
-    end
-
-    self.map_simple_action('app.activate') do
-    end
-  end
+ end
 
   def handle_activate()
-    window = VtyAppWindow.new(self)
-    window.present
+      Kernel.warn("Handling activate for #{self.class}: #{self.inspect}",
+                  uplevel: 1)
+      window = create_app_window
+      window.present
+  end
+
+  ## the preferences window for this application,
+  ## if any preferences window is active
+  attr_accessor :prefs_window
+
+  ## create a new application preferences window for this application
+  ##
+  ##
+  ## at most one preferences window should be visible for a single
+  ## appliction instance
+  def create_prefs_window(transient_for = self.active_window)
+    wdw = VtyPrefsWindow.new(transient_for: transient_for)
+    wdw.signal_connect_after("destroy") do
+        self.prefs_window = nil
+    end
+    return wdw
+  end
+
+  def create_app_window()
+    VtyAppWindow.new(self)
   end
 
   def config
@@ -310,39 +431,40 @@ class VtyApp < Gtk::Application
   end
 
   def run()
+    ## reached before #start
+    Kernel.warn("Registering #{self}", uplevel: 0)
+    self.register
+    Kernel.warn("Start for #{self}", uplevel: 0)
     self.start
+    # Kernel.warn("Activating #{self}", uplevel: 0)
+    # self.activate
+    # Kernel.warn("Calling superclass run mtd for #{self}", uplevel: 0)
     super
   end
 
+  ## called from #activate as defined via inclusion of GtkAppPrototype
+  ##
   def start(args = nil)
     ## args would not be handled here, should typically be empty
     if args && (! args.empty?)
       Kernel.warn("Discarding args: #{args.inspect}", uplevel: 0)
     end
 
+    Kernel.warn("Starting #{self} with default shell #{self.default_shell}",
+                uplevel: 0)
 
-    if @running
-      Kernel.warn("already running: #{@running.inspect}", uplevel: 1)
-    else
-      ## FIXME first map a handler for the "startup" signal
-      ## in this application, before self.register
-      begin
-
-        self.class.started = self
-
-        # self.register ## glib. may activate the 'startup' signal
-        ## ^ TBD where is GApplication#register being called when it's not called here?
-
-      rescue Gio::IOError::Exists => e
-        ## TBD - determining what other application instance is already registerd
-        ## while this duplicate application instance cannot be registered
-        raise e
-      end
+    begin
+      self.class.started = self
+      ## calling this class' own #register directly
+      ##
+      ## in a superclass method, this may activate the application's
+      ## 'startup' signal by side effect
+      self.register
+    rescue Gio::IOError::Exists => e
+      raise e
     end
+
     ## TBD dispatching for run <...> start here
   end
 end
-
-$APP = VtyApp.new
-$APP.activate
 
