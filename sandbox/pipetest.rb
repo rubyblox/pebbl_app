9d951adcc8b076576567df33e66b31454dc70e17
Sean Champ <spchamp@users.noreply.github.com>
2022-06-19 19:19:22 -0700

    Updates for pipetest.rb, ptytest.rb
    piptest.rb:
    - dispatching for I/O on output streams in one call to IO.select
    - removing unused sched mutex, sched cv, io mutex
    - ensuring output from test msg calls is tagged "--"
    - adding delays for sync
    
    ptytest.rb
    - First prototype for a PtyCmd implementation (no Gtk Integration)

--- sandbox/pipetest.rb
+++ sandbox/pipetest.rb
@@ -1,10 +1,9 @@
 ## pipe-mode IO with GLib::Spawn
 
 require 'gtk3'
-
 Gtk.init if Gtk.respond_to?(:init)
 
-STDERR.puts "Is a TTY: #{STDIN.isatty.inspect}"
+STDERR.puts "stdin is a TTY: #{STDIN.isatty.inspect}"
 
 ## FIXME needs port for outside of GLib, using a similar async I/O handling
 ## and implementing the call to the setup function in the subprocess
@@ -35,7 +34,7 @@ pid, p_in, p_out, p_err =
   ##
   ## - do not write to ENV here
   ##
-  ## - avoid any calls that must rely on mutexes held in the calling process
+  ## - avoid any calls that would rely on mutexes held in the calling process
   ##
   ## - from Linux signal(7) (openSUSE Tumbleweed 20220611, kernel 5.18.2-1)
   ##   apropos signal handlers and fork:
@@ -43,9 +42,6 @@ pid, p_in, p_out, p_err =
   ##   "A child created via fork(2) inherits a copy of its parent's signal dispositions.
   ##    During an execve(2), the dispositions of handled signals are reset to the default;
   ##    the  dispositions of ignored signals are left unchanged."
-  ##
-  ## - pthread docs TBD @ thread inheritance per POSIX / OS
-  ##   - in Ruby (??) pthread_atfork(2)
   ##
   ## - Linux signal-safety(7) (openSUSE Tumbleweed, same release info)
   ##
@@ -59,7 +55,7 @@ pid, p_in, p_out, p_err =
   STDIN.close_on_exec = false
   STDOUT.close_on_exec = false
   STDERR.close_on_exec = false
-  STDERR.puts "Starting shell ..."
+  STDERR.puts "Starting shell"
   ## flush any stream that was accessed in the setup function...
   STDERR.flush
   ## FIXME if yielding to an arbitrary setup function here,
@@ -67,40 +63,21 @@ pid, p_in, p_out, p_err =
   ## then clean up and exit(-1) in the rescue block
   }
 
-sched_mtx = Mutex.new
-cv = ConditionVariable.new
-out_mtx = Mutex.new
-
-
-## async i/o for separate output (pty) and error (pipe) streams in the subprocess
-##
-## orthogonal to the process fork=>exec implementation
-##
-## FIXME this implementation may ilustrate nondeterministic behaviors,
-## as to which reader thread will display its output first.
-##
-## at least, each reader thread should have independent access to the
-## primary output (here, warn => stderr) in this implementation
-##
-## alternate approach: Use one thread, select on err and out
-## - cf ./ptytest.rb
 
 Thread.new {
   begin
-    while ! p_err.closed?
-      if IO.select([p_err], nil, nil, 0)
-        begin
-          text = p_err.readpartial(2048)
-          sched_mtx.synchronize {
+    while ! p_in.closed?
+      if (selected = IO.select([p_err, p_out], nil, nil, 0))
+        selected.each do |avl|
+          avl.each do |io|
             begin
-              out_mtx.try_lock || cv.wait(sched_mtx)
-              Kernel.warn("Read from err #{p_err}: #{text}")
-              cv.signal
-              text = "".freeze
-            ensure
-              out_mtx.unlock
+              text = io.readpartial(2048)
+              Kernel.warn(
+                "-- Read from %s\n%s" % [
+                  (io == p_err ? :err : :out), text
+                ])
             end
-          }
+          end
         end
       end
     end
@@ -108,43 +85,20 @@ Thread.new {
     Kernel.warn(e)
     Thread.current.exit
   end
-  Kernel.warn("err closed")
+  Kernel.warn("-- input closed")
 }
 
-Thread.new {
-  begin
-    while ! p_out.closed?
-      if IO.select([p_out], nil, nil, 0)
-        text = p_out.readpartial(2048)
-        sched_mtx.synchronize {
-          begin
-            out_mtx.try_lock || cv.wait(sched_mtx)
-            Kernel.warn("Read from out #{p_out}: #{text}")
-            cv.signal
-            text = "".freeze
-          ensure
-            out_mtx.unlock
-          end
-        }
-      end
-    end
-  rescue EOFError, IOError => e
-    ## TBD will any partial read before EOFError be lost here?
-    ## or would IO#readpartial return normally at eof,
-    ## if there was any data read?
-    Kernel.warn(e)
-    Thread.current.exit
-  end
-  Kernel.warn("out closed")
-}
+## delay for synch
+sleep 0.1
 
-Kernel.warn("Write to #{p_in}")
+Kernel.warn("-- Writing to pty")
 p_in.puts 'echo bash ${BASH_VERSION}'
 p_in.flush ## this
 
-sleep 3
+## delay for synch
+sleep 0.1
 
-Kernel.warn("close")
+Kernel.warn("-- close")
 p_in.close
 p_out.close
 p_err.close
--- sandbox/ptytest.rb
+++ sandbox/ptytest.rb
@@ -4,160 +4,339 @@
 ## PebblApp::Support::PtyCmd
 ## and subsq PebblApp::GtkSupport::VtyCmd
 
-require 'gtk3'
-
-Gtk.init if Gtk.respond_to?(:init)
-
-#STDERR.puts "Is a TTY: #{STDIN.isatty.inspect}"
-
+require 'pebbl_app/support'
 require 'pty'
-ctl_io, pty_io = PTY.open
 
-## FIXME the ctl_io stream can accept input. For the calling process,
-## this should be integrated with scheduling for reading subprocess
-## output from the pty channel or the error pipe, blocking the read
-## internally until output has completed, and blocking the write until
-## any input has completed within the calling process
+## for an EOL test (FIXME should be stored in constants)
+require 'stringio'
 
+module PebblApp::Support
 
-## with Gtk support now ..
-require 'vte3'
-Vte::Pty.new(ctl_io.fileno)
-## ^ more or less the only feature that would differ for this impl,
-##   when using Gtk/GLib support here
-
-## NB an encoding arg is accepted on IO.pipe
-##
-## Add'l concerns for encoding support
-## - The subprocess cmd may configure an encoding internally,
-##   such that may be affected by some environment settings e.g LC_ALL
-## - The section after fork and before exec will inherit any encoding
-##   for streams open at the time of fork
-##
-## This initial implementation will use the default encoding in the Ruby
-## process, at time of call
-
-err_pipe_rd, err_pipe_wr = IO.pipe
-
-sub_env = ENV
-
-## marker for an instance variable,
-## initialized here for any usage
-## in the subprocess, on exec fail
-$EXITSTATUS = 0
-
-if (pid = Process.fork)
-  ## in the calling process
-  Kernel.warn("Forked process #{pid}")
-  ## configure some local variables for the pty channel
-  ## and error pipe streams
-  p_out = ctl_io
-  p_in = ctl_io
-  p_err = err_pipe_rd
-  ## cleanups for streams
-  pty_io.close
-  err_pipe_wr.close
-  ### handler for post-exec in the calling process
-  ## Vte::Terminal...watch_proc(pid)
-else
-  ## subprocess setup section
-  ## - simlar to the setup function for GLib::Spawn.async* via Ruby-GNOME GLib
-  ##
-  ## FIXME see docs for notes on limitations & concerns for the section
-  ## betwen fork and exec in portable applications
-  ##
-  ## FIXME this API could support a user-provided setup block here,
-  ## additional to any user-provided "error in exec" block,
-  ## and any user-provided post-exec block for the calling process
-  ## and any user-provided IO handling blocks in the calling process
-  ##
-  pty_io.close_on_exec = false
-  err_pipe_wr.close_on_exec = false
-  STDERR.close_on_exec = true
-  STDOUT.close_on_exec = true
-  STDIN.close_on_exec = true
-  STDOUT.flush
-  STDERR.flush
-  STDIN.flush
-  pty_io.flush
-  err_pipe_wr.puts "In: #{Process.pid}"
-  err_pipe_wr.puts "Starting shell ..."
-  ## flush any streams accessed directly
-  err_pipe_wr.flush
-  ## TBD side effects (??) for I/O scheduling in the calling process,
-  ## with or without flushing other streams here,
-  ## or simply the concern as to whether the err reader or out reader
-  ## displays its output first in the calling process
-  ## using an exit status that can be set in any exec_error handler
-  ## FIXME once implemented, store this in an instance attr/ivar
-  $EXITSTATUS = -1
-  begin
-    Process.exec(sub_env, "bash", in: pty_io, out: pty_io, err: err_pipe_wr)
-  rescue
-    Kernel.warn($!, uplevel: 0)
+  module Const
+    EOL ||= proc { io = StringIO.new; io.puts; io.string }.call
   end
-  ## exec failed, or this would not be reached
-  ##
-  ## clean up & exit with any configured exit status code
-  err_pipe_wr.flush
-  err_pipe_wr.close
-  pty_io.flush
-  pty_io.close
-  STDERR.flush
-  STDERR.close
-  STDOUT.flush
-  STDOUT.close
-  STDIN.flush
-  STDIN.close
-  exit($EXITSTATUS)
+
+ class PtyCmd
+   attr_reader :input_handler, :stderr_handler, :stdout_handler, :stdout_handlers
+   attr_reader :cmd, :env, :pid, :output_thread
+
+   ## Controlling IO channel for the PTY, for input to the subprocess
+   ## and standard output from the subprocess
+   attr_reader :pty
+
+   ## IO channel for standard error output from the subprocess
+   attr_reader :error_channel
+
+
+   def initialize(cmd = (ENV['SHELL'] || "/bin/sh"),
+                  env: ENV)
+     @cmd = cmd
+     @env = env
+     @output_handlers = {}
+     @output_streams = {}
+     @exitfail = -1
+     @sched_mtx = Mutex.new
+     @sched_cv = ConditionVariable.new
+     @io_mtx = Mutex.new
+   end
+
+   def with_stderr_handler(&block)
+    with_output_handler(:err, &block)
+   end
+   def with_stdout_handler(&block)
+    with_output_handler(:out, &block)
+   end
+
+   def with_output_handler(tag, &block)
+     @output_handlers[tag] = block
+   end
+
+   def with_pid_init(&block)
+     @pid_init_handler = block
+   end
+
+   def with_subprocess_init(&block)
+     @subprocess_init_handler = block
+   end
+
+   def with_exec_error_handler(&block)
+     @exec_error_handler = block
+   end
+
+
+   def send(text)
+     with_io_sched do
+       @pty.puts(text)
+       ## FIXME even calling flush after each send, the shell response
+       ## is still delayed with the I/O methodology used here
+       @pty.flush
+     end
+   end
+
+   def handle_output()
+     ## cache instance variables for local lookup
+     streams = @output_streams
+     streams_io = streams.keys
+     handlers = @output_handlers
+     ## read until first of EOF, IO error, or exception in a handler
+     while true
+       readable = nil
+       if (readable = IO.select(streams_io, nil, nil, 0))
+         readable.each do |ios|
+           ## arrays encapsulating arrays. iterate on each ..
+           ##
+           ## this should result in a predictable order of I/O
+           ios.each do  |io|
+             with_io_sched do
+               if ((tag = streams[io]) && (hdlr = handlers[tag]))
+                 hdlr.yield(io, tag)
+               else
+                 raise "Unable to find stream for %s %s"  % [
+                   io, (tag ? ("with tag " + tag.to_s) : "(tag unknown)")
+                 ]
+               end
+             end
+           end
+         end
+       end
+     end
+   end
+
+   def handle_output_async()
+     Thread.new {
+       begin
+         self.handle_output
+       rescue EOFError, IOError # => e
+         ## FIXME use any mapped handlers in the implementation
+         # Kernel.warn(e)
+         Thread.current.exit
+       ensure
+         ## close the subprocess after I/O error with thhe subprocess
+         @output_streams.each do |elt|
+           ## close the PTY, error channel pipe, and any other streams
+           elt.first.close
+         end
+         ## FIXME this needs to be configurable with callback
+         ## e.g handle_output_eror
+         Process.kill("TERM", self.pid)
+       end
+     }
+   end
+
+
+   def init_subprocess
+     ## initialize a controlling PTY for the subprocess
+     ctl_io, pty_io = PTY.open
+     ## initialize a pipe for distinct dispatch on error output
+     ## from the subprocess
+     err_pipe_rd, err_pipe_wr = IO.pipe
+
+     ## @exitfail is used only in this method's representation in the
+     ## subprocess, if the call to exec fails.
+     ##
+     ## The variable should be initialized before fork.
+     ##
+     ## The value of this instance variable may be changed within any
+     ## exec error handler in the subprocess, before the subprocess
+     ## exits with this exit code
+     @exitfail = -1
+
+     ## variables to initialize for the subprocess, before fork,
+     ## for availability to subprocess init handlers, after fork and
+     ## before exec.
+     ##
+     ## overwritten again in the calling process, after fork
+     @error_channel = err_pipe_wr
+     @pty = pty_io
+
+
+     if (pid = Process.fork)
+       @pid = pid
+       ## special streams
+       @pty = ctl_io
+       @error_channel = err_pipe_rd
+       ## close streams unused in the calling process
+       pty_io.close
+       err_pipe_wr.close
+       ## add streams to dispatch for output from the subprocess
+       @output_streams[err_pipe_rd] = :err
+       @output_streams[ctl_io] = :out
+       ## disapatch to any init callback
+       self.handle_pid_init(pid)
+       ## return the PID
+       return pid
+     else
+       ## subprocess init
+       pty_io.close_on_exec = false
+       err_pipe_wr.close_on_exec = false
+       STDERR.close_on_exec = true
+       STDOUT.close_on_exec = true
+       STDIN.close_on_exec = true
+       STDOUT.flush
+       STDERR.flush
+       STDIN.flush
+       begin
+         ## initialize the subprocess and then call exec,
+         ## exiting if on failure in either call
+         self.handle_subprocess_init()
+         Process.exec(env, cmd, in: pty_io, out: pty_io, err: err_pipe_wr)
+       rescue
+         begin
+           ## the exec error handler may update the @exitfail instance variable.
+           handle_exec_error($!)
+         rescue
+           ## nop. Ensure normal exit if there was an error in the handler
+         end
+         ## clean up & exit with any configured exit status code
+         err_pipe_wr.flush
+         err_pipe_wr.close
+         pty_io.flush
+         pty_io.close
+         STDERR.flush
+         STDERR.close
+         STDOUT.flush
+         STDOUT.close
+         STDIN.flush
+         STDIN.close
+         exit(@exitfail)
+       end
+     end
+   end
+
+   def run_async()
+     ## FIXME not reentrant
+     self.init_subprocess
+     @output_thread = self.handle_output_async
+   end
+
+   protected
+
+   attr_reader :sched_mtx, :sched_cv, :io_mtx
+
+   def with_io_sched(&block) ## FIXME define as protected
+     self.sched_mtx.synchronize {
+       ## FIXME timeout for sched_cv.wait & handling on timeout
+       ##
+       ## using the lock state of the io_mtx
+       ## as a "work available" condition
+       ## for this application onto condition variables
+       (got_mtx = self.io_mtx.try_lock) || self.sched_cv.wait(sched_mtx)
+       begin
+         block.yield(Thread.current)
+       ensure
+         if got_mtx
+           self.io_mtx.unlock
+         else
+           self.sched_cv.signal
+         end
+       end
+     }
+   end
+
+   def handle_pid_init(pid)
+     if (instance_variable_defined?(:@pid_init_handler))
+       block = instance_variable_get(:@pid_init_handler)
+       block.yield(pid)
+     end
+   end
+
+   def handle_subprocess_init()
+     if (instance_variable_defined?(:@subprocess_init_handler))
+       block = instance_variable_get(:@subprocess_init_handler)
+       block.yield()
+     end
+   end
+
+   def handle_exec_error(exception)
+     if (instance_variable_defined?(:@exec_error_handler))
+       block = instance_variable_get(:@exec_error_handler)
+       block.yield(exception)
+     end
+   end
+
+ end
+
+
+ ## ad hoc tests
+
+def Object.test_pty_cmd()
+
+ it = PtyCmd.new(%(bash))
+
+ it.with_pid_init do |pid|
+   Kernel.warn "#{File.basename($0)}: Initialized procecss #{pid}"
+ end
+
+ it.with_subprocess_init do
+   it.error_channel.puts "Initializing #{Process.pid}"
+ end
+
+ it.with_stderr_handler do |io|
+   text = ""
+   begin
+     while (c = io.readchar)
+       text << c
+       break if c == Const::EOL
+     end
+   rescue EOFError, IOError # => e
+     exit
+   ensure
+     if ! text.empty?
+       Kernel.warn("read from error channel: #{text}")
+     end
+   end
+ end
+
+
+ it.with_stdout_handler do |io|
+   text = ""
+   begin
+     while (c = io.readchar)
+       text << c
+       break if c == Const::EOL
+     end
+     #   text = io.readpartial(512)
+   rescue EOFError, IOError # => e
+     exit
+   ensure
+     if ! text.empty?
+       Kernel.warn("read from output channel: #{text}")
+     end
+   end
+ end
+
+ it.with_exec_error_handler do |exc|
+   Kernel.warn(exc, uplevel: 1)
+ end
+
+ it.run_async
+
+# in_thr = Thread.new {
+   ## testing i/o synchronization
+   ## - a PTY cmd without a separate stderr channel might be more ideal
+   ##   for e.g bash
+   ##
+   it.send("# input to shell, via PtyCmd")
+   it.send('echo "using bash ${BASH_VERSION}"')
+   sleep 0.01 ## pause to allow the output to display before next cmd
+   it.send('echo bye 1>&2')
+   it.send('echo "# exiting"')
+   sleep 0.01 ## pause to avoid loosing output while the shell exits
+   it.send('exit')
+# }
+# in_thr.join
+ it.output_thread.join
 end
 
-## async i/o for separate output (pty) and error (pipe) streams in the subprocess
+# ## TBD threads, Gtk, and Vte Pty support  ..
+# require 'gtk3'
+# Gtk.init if Gtk.respond_to?(:init)
+# require 'vte3'
+# Vte::Pty.new(ctl_io.fileno)
+# ## ^ more or less the only feature that would differ for this impl,
+# ##   when using Gtk/GLib support here
 ##
-## orthogonal to the process fork=>exec implementation
+## in pid init  ..
+## Vte::Terminal...watch_proc(pid)
 
-Thread.new {
-  begin
-    while ! p_err.closed?
-      readable = nil
-      if (readable = IO.select([p_err, p_out], nil, nil, 0))
-        readable.each do |tbd|
-          ## arrays encapsulating arrays. iterate on each ..
-          ##
-          ## this should result in a predictable order of I/O
-          ## via Kernel.warn
-          tbd.each do  |io|
-          ## TBD make the readpartial extent a configurable field
-          text = io.readpartial(512)
-          Kernel.warn("Read from #{io}: #{text}")
-          end
-        end
-      end
-    end
-  rescue EOFError, IOError => e
-    ## FIXME use any mapped handlers in the implementation
-    Kernel.warn(e)
-    Thread.current.exit
-  ensure
-    pty_io.close
-    ctl_io.close
-    err_pipe_wr.close
-    err_pipe_rd.close
-    Process.kill("TERM", pid)
-  end
-  Kernel.warn("err closed")
-}
-
-
-Kernel.warn("Write to #{p_in}")
-p_in.puts 'echo bash ${BASH_VERSION}'
-p_in.flush ## this
-
-## a pause is needed in this test,
-## to prevent loosing the i/o to/from the subprocess
-sleep 0.1
-
-Kernel.warn("close")
-p_in.close
-p_out.close
-p_err.close
+end ## module
